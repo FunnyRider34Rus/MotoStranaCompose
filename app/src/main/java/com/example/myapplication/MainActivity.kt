ed880d64a150e91d1aef77fb0801bca9bb884b05
FunnyRider34Rus <elpablo.volgograd@yandex.ru>
2022-08-30 23:53:44 +0300

    dashboard

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -9,7 +9,10 @@ import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
+import com.example.myapplication.database.NODE_NEWS
+import com.example.myapplication.database.REMOTE_DATABASE
 import com.example.myapplication.database.initFirebase
+import com.example.myapplication.models.Event
 import com.example.myapplication.ui.navigation.RootNavGraph
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
@@ -22,7 +25,20 @@ class MainActivity : ComponentActivity() {
             ACTIVITY_CONTEXT = this
             initFirebase()
             App()
+            //Служебная функция для записи событий в базу данных
+            //recordToDBEvent()
         }
+    }
+
+    private fun recordToDBEvent() {
+        val event = Event (
+            "2022-08-28",
+            "Royal Enfield начал выпуск Hunter 350",
+            "Классический родстер в индийском исполнении будет производиться в двух версиях: Retro и Metro",
+            "Причём, в данном случае можно смело говорить не о ретро-стилистике, а о настоящем ретро-мотоцикле, производимом в наши дни. Royal Enfield Hunter 350 оснащается 350-кубовым одноцилиндровым двигателем, знакомым по моделям Meteor и Classic 350. Оснащённый системой впрыска топлива, мотор развивает скромные 20 л.с. при 6100 об/мин, пик крутящего момента 25,6 Нм приходится на 4000 об/мин.\nHunter 350 выпускается в двух вариантах исполнения: Retro или Metro. Версия Retro выделяется спицованными 17-дюймовыми колёсами, в то время как Metro оснащается легкосплавными дисками такой же размерности. Больше глобальных различий между версиями нет, отличаются только цвето-графические схемы.\nА знаете, сколько стоит Royal Enfield Hunter 350 в Индии? Версия Retro в расцветке Factory Black или Factory Silver обойдётся в 149 900 рупий, что по сегодняшнему курсу эквивалентно 113 000 рублей. Исполнение Metro, доступное в цветах Dapper White, Dapper Ash и Dapper Grey, будет стоить чуть дороже, от 163 900 рупий, т.е. около 124 000 рублей. Самыми же дорогими будут Hunter 350 Metro в расцветках Rebel Black, Rebel Blue и Rebel Red — цена на них будет 168 900 рупий, т.е. примерно 127 000 рублей.",
+            "https://moto-magazine.ru/news/2022/08/2023-royal-enfield-hunter-350---rebel-blue.jpg"
+                )
+        REMOTE_DATABASE.child(NODE_NEWS).child(event.date).setValue(event)
     }
 }
 
--- app/src/main/java/com/example/myapplication/ui/screens/dashboard/Dashboard.kt
+++ app/src/main/java/com/example/myapplication/ui/screens/dashboard/Dashboard.kt
@@ -1,26 +1,41 @@
 package com.example.myapplication.ui.screens.dashboard
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Tab
 import androidx.compose.material.TabRow
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import coil.compose.AsyncImage
 import com.example.myapplication.R
+import com.example.myapplication.database.NODE_EVENT
+import com.example.myapplication.database.NODE_NEWS
 import com.example.myapplication.ui.theme.MyApplicationTheme
 import com.example.myapplication.ui.theme.black
 import com.example.myapplication.ui.theme.white
 import androidx.compose.material.Text as Text
 
 @Composable
-fun Dashboard(navController: NavHostController = rememberNavController()) {
+fun Dashboard(
+    navController: NavHostController = rememberNavController()) {
 
     var state by remember { mutableStateOf(0) }
     val titles = listOf(stringResource(id = R.string.news), stringResource(id = R.string.events))
+    val events by remember { mutableStateOf(EventsViewModel()) }
+
+    events.getEvents(NODE_NEWS)
 
     Column {
+        //Табсы с новостями и мероприятиями
         TabRow(
             selectedTabIndex = state,
             backgroundColor = white,
@@ -29,9 +44,34 @@ fun Dashboard(navController: NavHostController = rememberNavController()) {
             titles.forEachIndexed { index, title ->
                 Tab(
                     selected = state == index,
-                    onClick = { state = index },
+                    onClick = {
+                        state = index
+                        when (state) {
+                            0 -> events.getEvents(NODE_NEWS)
+                            1 -> events.getEvents(NODE_EVENT)
+                        }
+                    },
                     text = { Text(text = title) }
                 )
+            }
+        }
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            itemsIndexed(events.events) { _, item ->
+                Card(modifier = Modifier.padding(horizontal = 0.dp, vertical = 16.dp)) {
+                    Column {
+                        AsyncImage(
+                            model = item.image,
+                            contentDescription = null)
+                        Text(text = item.title_text, style = MaterialTheme.typography.h1)
+                        Text(text = item.header_text, style = MaterialTheme.typography.h3)
+                    }
+                }
             }
         }
     }
